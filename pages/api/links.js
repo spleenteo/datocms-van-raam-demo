d8ccd85361a7a5bcebed3509f778ee79da24dd29
Matteo Papadopoulos <m.papadopoulos@cantierecreativo.net>
2022-10-27 09:47:32 +0200

    Preview con ste

---
+++ pages/api/links.js
@@ -0,0 +1,41 @@
+// Put this code in the following path of your Next.js website:
+// /pages/api/preview/links.js
+// this "routing" function knows how to convert a DatoCMS record
+// into canonical URL within the website
+const generatePreviewLink = ({ item, itemType, locale }) => {
+  const localePrefix = locale === "en" ? "" : `/${locale}`;
+  switch (itemType.attributes.api_key) {
+    case "post":
+      return {
+        label: "Loclahost",
+        url: `/posts/${item.attributes.slug}`,
+      };
+    default:
+      return null;
+  }
+};
+const handler = (req, res) => {
+  // setup CORS permissions
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Content-Type", "application/json");
+  // This will allow OPTIONS request
+  if (req.method === "OPTIONS") {
+    return res.status(200).send("ok");
+  }
+  const previewLink = generatePreviewLink(req.body);
+  if (!previewLink) {
+    return res.status(200).json({ previewLinks: [] });
+  }
+  const previewLinks = [
+    previewLink,
+    // we generate the Preview Mode URL:
+    {
+      label: `${previewLink.label} - Preview`,
+      url: `http://localhost:3000/api/preview?redirect=${previewLink.url}`,
+    },
+  ];
+  return res.status(200).json({ previewLinks });
+};
+export default handler;
--- pages/api/preview.js
+++ pages/api/preview.js
@@ -5,10 +5,10 @@ export default async (req, res) => {
 
   const secret = process.env.NEXT_EXAMPLE_CMS_DATOCMS_PREVIEW_SECRET;
 
-  // Check the secret and next parameters
-  if (secret && req.query.secret !== secret) {
-    return res.status(401).json({ message: "Invalid token" });
-  }
+  // // Check the secret and next parameters
+  // if (secret && req.query.secret !== secret) {
+  //   return res.status(401).json({ message: "Invalid token" });
+  // }
 
   // Enable Preview Mode by setting the cookies
   res.setPreviewData({});
